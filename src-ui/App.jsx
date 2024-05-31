3713c25fa2aebe0aadab45350c31028aaa8f69af
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-05-31 12:40:17 +0900

    react test

--- src-ui/App.jsx
+++ src-ui/App.jsx
@@ -1,29 +1,33 @@
-    import { useState } from "react";
-    import reactLogo from "./assets/react.svg";
-    import { invoke } from "@tauri-apps/api/tauri";
-    import "./App.css";
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
 
-    function App() {
+import { MainFunctionSwitch } from "@MainFunctionSwitch";
+
+function App() {
     const [greetMsg, setGreetMsg] = useState("");
     const [name, setName] = useState("");
 
     async function greet() {
         // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+        // setGreetMsg(await invoke("run_python_script"));
         setGreetMsg(await invoke("run_python_script", { input: name }));
     }
 
     return (
         <div className="container">
+        <MainFunctionSwitch />
         <h1>Welcome to Tauri!</h1>
-
         <div className="row">
-            <a href="https://vitejs.dev" target="_blank">
+
+            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
             <img src="/vite.svg" className="logo vite" alt="Vite logo" />
             </a>
-            <a href="https://tauri.app" target="_blank">
+            <a href="https://tauri.app" target="_blank" rel="noreferrer">
             <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
             </a>
-            <a href="https://reactjs.org" target="_blank">
+            <a href="https://reactjs.org" target="_blank" rel="noreferrer">
             <img src={reactLogo} className="logo react" alt="React logo" />
             </a>
         </div>
---
+++ src-ui/windows/main_window/main_function_switch/MainFunctionSwitch.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./MainFunctionSwitch.module.scss";
+
+export const MainFunctionSwitch = () => {
+    return (
+        <div className={styles["container"]}>
+            <p className={styles["label"]}>React Test</p>
+        </div>
+    )
+}
