792f60c19cae3a71e7da16e7003f3852cf1c7457
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-06-04 08:38:54 +0900

    [WIP] python側、スレッドにするしない関係なかったことに気づいたのでmainに戻した。

--- src-python/test.py
+++ src-python/test.py
@@ -1,28 +1,21 @@
 import sys
 import json
 import datetime
-import threading
 import time
-
-
-def process_input():
-    while True:
-        input_data = sys.stdin.readline().strip()
-        if not input_data:
-            break
-
-        with open('process.log', 'a') as f:
-            f.write(f"input_data: {input_data}\n")
-
-        if input_data:
-            response = f"Hello, {input_data} from Python!"
-            print(response, flush=True)
-
 
 
 def main():
     # 標準入力からデータを受け取る
     print("Init", flush=True)
+
+    input_data = sys.stdin.readline().strip()
+
+    with open('process.log', 'a') as f:
+        f.write(f"input_data: {input_data}\n")
+
+    if input_data:
+        response = f"Hello, {input_data} from Python!"
+        print(response, flush=True)
 
 
     # input_data = sys.stdin.readline().strip()
@@ -33,23 +26,21 @@ def main():
     #     response = f"Hello, {input_data} from Python!"
     #     print(response, flush=True)
 
-    received_data = "test"
-    response = "Hello, test from Python!"
+    # received_data = "test"
+    # response = "Hello, test from Python!"
 
-    with open('process.log', 'a') as f:
-        f.write(f"Received data: {received_data}\n")
-        f.write(f"Response: {response}\n")
+    # with open('process.log', 'a') as f:
+    #     f.write(f"Received data: {received_data}\n")
+    #     f.write(f"Response: {response}\n")
 
-    # 標準出力に結果を出力
-    for i in range(10):
-        print(str(i), flush=True)
-        time.sleep(1)
+    # # 標準出力に結果を出力
+    # for i in range(10):
+    #     print(str(i), flush=True)
+    #     time.sleep(1)
 
 if __name__ == "__main__":
     try:
-        input_thread = threading.Thread(target=process_input)
-        input_thread.start()
-        # main()
+        main()
     except Exception:
         import traceback
         with open('error.log', 'a') as f:
