1ca55997fb91baf263d99674687173d50f7e5c64
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-06-06 11:23:09 +0900

    [Update] 処理を分割 main.rsの要らないコードの削除 Eslint: 	no-unreachable: セミコロンルール追加（なんとなく 今後調整予定） 	no-unused-vars: 使っていない変数があれば警告に（エラーではない）

--- src-ui/App.jsx
+++ src-ui/App.jsx
@@ -1,19 +1,27 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Command } from '@tauri-apps/api/shell';
-// import reactLogo from "./assets/react.svg";
-// import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
-// import { MainFunctionSwitch } from "@MainFunctionSwitch";
+import { MainFunctionSwitch as TestInputField } from "@MainFunctionSwitch";
+import { store } from "@store";
+import { asyncSendMessage } from "./logic";
 
 
-function App() {
-    const [message_from_python, setMessage] = useState("");
-    const [input_value, setInputValue] = useState("");
-    const childRef = useRef(null); // Ref to hold the child process
+const MessageDisplay = ({ message }) => {
+    return (
+        <div className="message-container">
+            <p className="message-container-label">Message From Python:</p>
+            <p className="message-container-text">{message}</p>
+        </div>
+    );
+};
+// const MemolizedMessageDisplay = memo(MessageDisplay);
 
+export const App = () => {
+    const [message_from_python, setMessage] = useState("React Initialization...");
+    // console.log("App");
     useEffect(() => {
-        (async () => {
+        const asyncPythonStartFunc = async () => {
             const command = Command.sidecar('bin/test');
             command.on('error', error => console.error(`error: "${error}"`));
             command.stdout.on('data', (line) => {
@@ -22,55 +30,25 @@ function App() {
             });
             command.stderr.on('data', line => console.error('stderr:', line));
             const child = await command.spawn();
-            childRef.current = child;
-        })();
+            store.child = child;
+        };
 
+        asyncPythonStartFunc();
     }, []);
 
-    const asyncSendMessage = async (value = input_value) => {
-        console.log(value);
-        // send to python
-        const child = childRef.current;
-        if (child) {
-            await child.write(value);
-        }
-    }
-
-    const onSubmitFunction = (e) => {
-        e.preventDefault();
-        asyncSendMessage();
-    }
-
-    const onChangeFunction = (e) => {
-        // e.preventDefault();
-        setInputValue(e.currentTarget.value);
-        asyncSendMessage(e.currentTarget.value);
-    }
+    const asyncButtonClicked = async () => {
+        console.log("asyncButtonClicked");
+        asyncSendMessage("Button has clicked")
+    };
 
 
     return (
         <div className="container">
-            {/* <MainFunctionSwitch /> */}
-            <form
-                className="message-form"
-                onSubmit={onSubmitFunction}
-            >
-                <div className="message-textarea-container">
-                    <textarea
-                    id="message-textarea"
-                    className="message-textarea"
-                    onChange={onChangeFunction}
-                    placeholder="Enter a message..."
-                    />
-                </div>
-                <button className="submit-button" type="submit">Submit</button>
-            </form>
-            <div className="message-container">
-                <p className="message-container-label">Message From Python:</p>
-                <p className="message-container-text">{message_from_python}</p>
-            </div>
+            <TestInputField />
+            <MessageDisplay message={message_from_python} />
+            <button type="button" onClick={asyncButtonClicked} className="just-button">Just Button</button>
+
+            {/* <MemolizedMessageDisplay message={message_from_python} /> */}
         </div>
     );
 }
-
-export default App;
--- src-ui/windows/main_window/main_function_switch/MainFunctionSwitch.jsx
+++ src-ui/windows/main_window/main_function_switch/MainFunctionSwitch.jsx
@@ -1,10 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./MainFunctionSwitch.module.scss";
+import { asyncSendMessage } from "../../../logic";
+
 
 export const MainFunctionSwitch = () => {
+    const [input_value, setInputValue] = useState("");
+    const onSubmitFunction = (e) => {
+        console.log("onSubmitFunction");
+        e.preventDefault();
+        asyncSendMessage(input_value);
+    }
+
+    const onChangeFunction = (e) => {
+        console.log("onChangeFunction");
+        // e.preventDefault();
+        setInputValue(e.currentTarget.value);
+        asyncSendMessage(e.currentTarget.value);
+    }
+
     return (
         <div className={styles["container"]}>
-            <p className={styles["label"]}>React Test</p>
+            <form
+                className="message-form"
+                onSubmit={onSubmitFunction}
+            >
+                <div className="message-textarea-container">
+                    <textarea
+                    id="message-textarea"
+                    className="message-textarea"
+                    onChange={onChangeFunction}
+                    placeholder="キーが入力されるたびに送信"
+                    />
+                </div>
+                <button className="submit-button" type="submit">Submit</button>
+            </form>
+            {/* <p className={styles["label"]}>React Test</p> */}
         </div>
     )
 }
