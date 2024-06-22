75ca828ea555f09c4d2edd636e30319423e1b424
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-06-22 15:55:09 +0900

    [Update] adjust VRCT logo

--- src-ui/windows/main_window/logo/Logo.jsx
+++ src-ui/windows/main_window/logo/Logo.jsx
@@ -2,8 +2,8 @@ import styles from "./Logo.module.scss";
 import vrct_logo from "@images/vrct_logo_for_dark_mode.png"
 export const Logo = () => {
     return (
-        <div>
-            <img src={vrct_logo} className={styles["logo"]} alt="React logo" />
+        <div className={styles["logo_container"]}>
+            <img src={vrct_logo} className={styles["logo"]} alt="VRCT logo" />
         </div>
     )
 }
