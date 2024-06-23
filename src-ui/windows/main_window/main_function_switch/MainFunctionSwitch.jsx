57834858585e24a4f2e709269af6f350068d8aad
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-06-23 23:37:13 +0900

    [Update] Add main function switch. for importing svg, add svgr(for vite). Change font family to Yu Gothic UI, for now.

--- src-ui/windows/main_window/main_function_switch/MainFunctionSwitch.jsx
+++ src-ui/windows/main_window/main_function_switch/MainFunctionSwitch.jsx
@@ -1,12 +1,41 @@
-// import styles from "./MainFunctionSwitch.module.scss";
+import styles from "./MainFunctionSwitch.module.scss";
+import TranslationSvg from "@images/translation.svg?react";
+import MicSvg from "@images/mic.svg?react";
+import HeadphonesSvg from "@images/headphones.svg?react";
+import ForegroundSvg from "@images/foreground.svg?react";
 
 export const MainFunctionSwitch = () => {
     return (
-        <div>
-            <p>Translation</p>
-            <p>Voice2Chatbox</p>
-            <p>Speaker2Log</p>
-            <p>Foreground</p>
+        <div className={styles["container"]}>
+            <SwitchContainer switch_label="Translation"image_component={
+                <TranslationSvg />
+            }/>
+            <SwitchContainer switch_label="Voice2Chatbox" image_component={
+                <MicSvg />
+            }/>
+            <SwitchContainer switch_label="Speaker2Log" image_component={
+                <HeadphonesSvg />
+            }/>
+            <SwitchContainer switch_label="Foreground" image_component={
+                <ForegroundSvg />
+            }/>
         </div>
-    )
+    );
+}
+
+import React from "react";
+const SwitchContainer = (props) => {
+    const imageWithClass = props.image_component
+        ? React.cloneElement(props.image_component, { className: styles["switch_image"] })
+        : null;
+
+    return (
+        <div className={styles["switch_container"]}>
+            <p className={styles["switch_label"]}>{props.switch_label}</p>
+            <div className={styles["switch_box"]}>
+                {imageWithClass}
+                <div className={styles["switch_indicator"]}></div>
+            </div>
+        </div>
+    );
 }
