62434eace3ba2bcdd957823a032b1ffa35ae3a1c
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-06-24 04:48:07 +0900

    [Update] add message input box and send button

--- src-ui/windows/main_window/message_box/MessageBox.jsx
+++ src-ui/windows/main_window/message_box/MessageBox.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import styles from "./MessageBox.module.scss";
+import { asyncSendMessage } from "@logic";
+import SendMessageSvg from "@images/send_message.svg?react";
+
+
 
 export const MessageBox = () => {
+    const [input_value, setInputValue] = useState("");
+    const onSubmitFunction = (e) => {
+        console.log("onSubmitFunction");
+        e.preventDefault();
+        asyncSendMessage(input_value);
+    }
+
+    const onChangeFunction = (e) => {
+        console.log("onChangeFunction");
+        // e.preventDefault();
+        setInputValue(e.currentTarget.value);
+        asyncSendMessage(e.currentTarget.value);
+    }
+
     return (
         <div className={styles["container"]}>
-            <p>Input Textfield</p>
-            <p>Send Message Button</p>
+            <div className={styles["message_box_wrapper"]}>
+                <textarea
+                // id="message-textarea"
+                className={styles["message_box_input_area"]}
+                onChange={onChangeFunction}
+                placeholder="Input Textfield"
+                />
+            </div>
+            <button className={styles["message_send_button"]} type="button" onClick={onSubmitFunction}>
+                <SendMessageSvg className={styles["message_send_icon"]} />
+            </button>
         </div>
     )
 }
