b475f8fb1507f319586cd373dc9f396c9d3bb694
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-06-24 19:08:02 +0900

    [Update] use Jotai for handle the UI state. and add message input box and temp log.

--- src-ui/store.js
+++ src-ui/store.js
@@ -1,3 +1,25 @@
+import {
+    atom,
+    useAtomValue,
+    useSetAtom
+} from "jotai";
+
 export const store = {
     child: null,
+};
+
+export const sentMessageList = atom(["default"]);
+
+export const useSentMessageList = () => {
+    const setSentMessageList = useSetAtom(sentMessageList);
+    const currentSentMessageList = useAtomValue(sentMessageList);
+
+    const updateSentMessageList = (message) => {
+        setSentMessageList((old_value) => [
+            ...old_value,
+            message,
+        ]);
+    };
+
+    return { updateSentMessageList, currentSentMessageList };
 };
--- src-ui/windows/main_window/log_box/LogBox.jsx
+++ src-ui/windows/main_window/log_box/LogBox.jsx
@@ -1,9 +1,19 @@
 // import styles from "./MainFunctionSwitch.module.scss";
+import { useAtomValue } from "jotai";
+
+
+import { sentMessageList } from "@store";
 
 export const LogBox = () => {
+    const sent_message_list = useAtomValue(sentMessageList);
+    // console.log(sent_message_list);
+    // sent_message_list.map(sent_message => console.log(sent_message));
     return (
         <div>
             <p>Log Box</p>
+            {sent_message_list.map(sent_message => {
+                return <p key={crypto.randomUUID()}>{sent_message}</p>;
+            })}
         </div>
     )
 }
