0047507a4d441741ef29808952e47910a1c82de8
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-07-02 09:56:27 +0900

    [Update] Move start python function to out side of React component. then, remove some codes that is no longer in use.

--- src-ui/App.jsx
+++ src-ui/App.jsx
@@ -1,21 +1,11 @@
-import { useEffect, useRef } from "react";
 import styles from "./App.module.scss";
 
 import { asyncPythonStartFunc } from "@logic";
 import { MainWindow } from "./windows/main_window/MainWindow";
 
+asyncPythonStartFunc();
+
 export const App = () => {
-    const effectRan = useRef(false);
-
-    useEffect(() => {
-        // prevent twice execute initial function when under React Strict mode.
-        if (!effectRan.current) {
-            asyncPythonStartFunc();
-        }
-
-        return () => effectRan.current = true;
-    }, []);
-
     return (
         <div className={styles.container}>
             <MainWindow />
