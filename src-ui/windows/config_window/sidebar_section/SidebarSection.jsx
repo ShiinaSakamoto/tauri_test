35df78c329e2a5866c22f840a78ccdb3b97b1468
Sakamoto Shiina <68018796+ShiinaSakamoto@users.noreply.github.com>
2024-07-22 10:35:17 +0900

    [Update] Config Window: Changes the section title in top bar when a tab is selected.

--- src-ui/windows/config_window/sidebar_section/SidebarSection.jsx
+++ src-ui/windows/config_window/sidebar_section/SidebarSection.jsx
@@ -1,42 +1,18 @@
-import { useTranslation } from "react-i18next";
-
 import styles from "./SidebarSection.module.scss";
 
 export const SidebarSection = () => {
-    const { t } = useTranslation();
     return (
         <div className={styles.container}>
             <div className={styles.tab_wrapper}>
-                <Tab
-                    tab_id="appearance"
-                    label={t("config_window.side_menu_labels.appearance")}
-                />
-                <Tab
-                    tab_id="translation"
-                    label={t("config_window.side_menu_labels.translation")}
-                />
-                <Tab
-                    tab_id="transcription"
-                    label={t("config_window.side_menu_labels.transcription")}
-                />
-                <Tab
-                    tab_id="vr"
-                    label={t("config_window.side_menu_labels.vr")}
-                />
-                <Tab
-                    tab_id="others"
-                    label={t("config_window.side_menu_labels.others")}
-                />
-                <Tab
-                    tab_id="advanced_settings"
-                    label={t("config_window.side_menu_labels.advanced_settings")}
-                />
+                <Tab tab_id="appearance" />
+                <Tab tab_id="translation" />
+                <Tab tab_id="transcription" />
+                <Tab tab_id="vr" />
+                <Tab tab_id="others" />
+                <Tab tab_id="advanced_settings" />
             </div>
             <div className={styles.separated_tab_wrapper}>
-                <Tab
-                    tab_id="about_vrct"
-                    label="About VRCT"
-                />
+                <Tab tab_id="about_vrct" />
             </div>
         </div>
     );
@@ -44,10 +20,11 @@ export const SidebarSection = () => {
 
 
 import clsx from "clsx";
-
+import { useTranslation } from "react-i18next";
 import { useSelectedConfigTab } from "@store";
 
 const Tab = (props) => {
+    const { t } = useTranslation();
     const { updateSelectedConfigTab, currentSelectedConfigTab } = useSelectedConfigTab();
     const onclickFunction = () => {
         updateSelectedConfigTab(props.tab_id);
@@ -62,7 +39,7 @@ const Tab = (props) => {
 
     return (
         <div className={tab_container_class_names} onClick={onclickFunction}>
-            <p className={styles.tab_text}>{props.label}</p>
+            <p className={styles.tab_text}>{t(`config_window.side_menu_labels.${props.tab_id}`)}</p>
             <div className={switch_indicator_class_names}></div>
         </div>
     );
